1f53ea0ac7ddcb36c10e786ce3dfe00e97c4f071
Sejal Shrivastava <120376257+sejshriv@users.noreply.github.com>
2024-04-22 22:43:17 +0530

    Generate a Random Number.js
    Generate a Random Number

---
+++ Generate a Random Number.js
@@ -0,0 +1,7 @@
+// generating  a random number
+const a = Math.random();
+console.log(a);
+
+// generating a random number
+const a = Math.floor(Math.random() * (10 - 1)) + 1;
+console.log(`Random value between 1 and 10 is ${a}`);
